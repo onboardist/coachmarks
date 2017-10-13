2d11fe05a9e732488c0df8a1ce91bca4f4c82023
Brian Hann <brian.hann@gmail.com>
2017-10-13 15:41:57 -0500

    Fix drag positioning

--- src/index.js
+++ src/index.js
@@ -241,6 +241,7 @@ function elementRect(node, offsetParent) {
     height: rect.height,
   };
 }
+window.elementRect = elementRect;
 
 function midPoint(x1, y1, x2, y2) {
   return [(x1 + x2) / 2, (y1 + y2) / 2];
